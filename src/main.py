c5ef5a55a72cb123c533078212f9226ceefbf7c8
M.D. Walters <mail@mdwalters.ml>
2022-09-28 18:39:04 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class AboutDialog(Gtk.AboutDialog):
     def __init__(self, parent):
         Gtk.AboutDialog.__init__(self)
         self.props.program_name = "Eight Ball"
-        self.props.version = "1.4.2"
+        self.props.version = "1.4.3"
         self.props.authors = ["M.D. Walters"]
         self.props.copyright = "© 2022 M.D. Walters"
         self.props.logo_icon_name = "ml.mdwalters.EightBall"
